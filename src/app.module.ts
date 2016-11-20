7bef9073acf8cff3519b63d29ea748a0f43cb3ee
spederiva@gmail.com <spederiva@gmail.com>
2016-11-21 00:50:45 +0200

    minor changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
-import {AppComponent} from "./app/app.component";
+import {AppComponent} from "./components/app/app.component";
 import {ShoppingListComponent} from "./components/shopping-list/shopping-list.component";
 
 import '../public/css/styles.css';
