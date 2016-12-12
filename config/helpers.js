fe3b327b23a0397fdedb5668962115e09297a1a2
spederiva@gmail.com <spederiva@gmail.com>
2016-12-13 00:27:05 +0200

    Merging vendor and polyfills

--- config/helpers.js
+++ config/helpers.js
@@ -4,7 +4,7 @@ var _root = path.resolve(__dirname, '..');
 function root(args) {
     args = Array.prototype.slice.call(arguments, 0);
 
-    console.log("root", path.join.apply(path, [_root].concat(args)));
+    console.log("root", args, path.join.apply(path, [_root].concat(args)));
 
     return path.join.apply(path, [_root].concat(args));
 }
