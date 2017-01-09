32b3e184e2b1384d125f603b5fa17891f087f77f
sebastianp <sebastian.pederiva@888random.com>
2017-01-09 15:05:59 +0200

    Routing

--- src/modules/newlist/components/details/details.component.ts
+++ src/modules/newlist/components/details/details.component.ts
@@ -1,4 +1,5 @@
-import {Component} from '@angular/core'
+import {Component, OnInit} from '@angular/core';
+import {Router, ActivatedRoute, Params} from '@angular/router';
 
 @Component({
     selector: '',
@@ -7,8 +8,31 @@ import {Component} from '@angular/core'
         <h1>
             Hello World!
         </h1>
+
+        <div *ngFor="let x of xxx">
+        [{{x}}]
+        </div>
     `
 })
-export class DetailsComponent{
+export class DetailsComponent implements OnInit{
+    constructor(
+        private router: Router,
+        private route: ActivatedRoute
+    ) {
+
+    }
+
+    xxx:Number[];
+
+    ngOnInit():void {
+        let route = this.route;
+        debugger;
+        console.log(route);
+
+        route.params.switchMap((params:Params)=>this.xxx.push(+params['id']));
+
+        debugger;
+        console.log(this.xxx)
+    }
 
 }
